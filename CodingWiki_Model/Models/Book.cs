0cb4a7f81770fdf114838913a26a099c0bbab36d
Bui Dang Duong <thuynt181998@gmail.com>
2025-03-24 10:19:50 +0700

    Add project files.

---
+++ CodingWiki_Model/Models/Book.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingWiki_Model.Models
+{
+    //Mỗi sách có một chi tiết(BookDetail).
+    //Mỗi sách có một nhà xuất bản(Publisher).
+    //Một sách có thể có nhiều tác giả thông qua bảng trung gian BookAuthorMap.
+    public class Book
+    {
+        public int BookId { get; set; }// khóa chính
+        public string Title { get; set; }
+        [MaxLength(20)]
+        [Required]
+        public string ISBN { get; set; }
+        public decimal Price { get; set; }
+        [NotMapped]
+        public string PriceRange { get; set; }// Thuộc tính không được ánh xạ vào database
+
+        public BookDetail BookDetail { get; set; } // Liên kết 1-1 với `BookDetail`
+        [ForeignKey("Publisher")]
+        public int Publisher_Id { get; set; } // Khóa ngoại đến `Publisher`
+        public Publisher Publisher { get; set; } // Đối tượng nhà xuất bản
+
+        public List<BookAuthorMap> BookAuthorMap{ get; set; } // Liên kết nhiều-nhiều với `Author`
+    } 
+}
---
+++ CodingWiki_Web/Controllers/BookController.cs
@@ -0,0 +1,182 @@
+﻿using CodingWiki_DataAccess.Data;
+using CodingWiki_Model.Models;
+using CodingWiki_Model.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+namespace CodingWiki_Web.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public BookController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            List<Book> objList = _db.Books.Include(u => u.Publisher)
+                .Include(u => u.BookAuthorMap).ThenInclude(u => u.Author).ToList();
+            return View(objList);
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            BookVM obj = new();
+
+            obj.PublisherList = _db.Publishers.Select(i => new SelectListItem
+            {
+                Text = i.Name,
+                Value = i.Publisher_Id.ToString()
+            });
+
+            if (id == null || id == 0)
+            {
+                //create
+                return View(obj);
+            }
+            //edit
+            obj.Book = _db.Books.FirstOrDefault(u => u.BookId == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Upsert(BookVM obj)
+        {
+            if (obj.Book.BookId == 0)
+            {
+                //create
+                await _db.Books.AddAsync(obj.Book);
+            }
+            else
+            {
+                //update
+                _db.Books.Update(obj.Book);
+            }
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Details(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            BookDetail obj = new();
+
+            //edit
+
+            obj = _db.BookDetails.Include(u => u.Book).FirstOrDefault(u => u.Book_Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Details(BookDetail obj)
+        {
+            if (obj.BookDetail_Id == 0)
+            {
+                //create
+                await _db.BookDetails.AddAsync(obj);
+            }
+            else
+            {
+                //update
+                _db.BookDetails.Update(obj);
+            }
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            Book obj = new();
+            obj = _db.Books.FirstOrDefault(u => u.BookId == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _db.Books.Remove(obj);
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult ManageAuthors(int id)
+        {
+            BookAuthorVM obj = new()
+            {
+                BookAuthorList = _db.BookAuthorMaps.Include(u => u.Author).Include(u => u.Book)
+                    .Where(u => u.Book_Id == id).ToList(),
+                BookAuthor = new()
+                {
+                    Book_Id = id
+                },
+                Book = _db.Books.FirstOrDefault(u => u.BookId == id)
+            };
+
+            List<int> tempListOfAssignedAuthor = obj.BookAuthorList.Select(u => u.Author_Id).ToList();
+
+            //NOT IN clause
+            //get all the authors whos id is not in tempListOfAssignedAuthors
+
+            var tempList = _db.Authors.Where(u => !tempListOfAssignedAuthor.Contains(u.Author_Id)).ToList();
+            obj.AuthorList = tempList.Select(i => new SelectListItem
+            {
+                Text = i.FullName,
+                Value = i.Author_Id.ToString()
+            });
+
+            return View(obj);
+        }
+
+
+        [HttpPost]
+        public IActionResult ManageAuthors(BookAuthorVM bookAuthorVM)
+        {
+            if (bookAuthorVM.BookAuthor.Book_Id != 0 && bookAuthorVM.BookAuthor.Author_Id != 0)
+            {
+                _db.BookAuthorMaps.Add(bookAuthorVM.BookAuthor);
+                _db.SaveChanges();
+            }
+            return RedirectToAction(nameof(ManageAuthors), new { @id = bookAuthorVM.BookAuthor.Book_Id });
+        }
+
+        [HttpPost]
+        public IActionResult RemoveAuthors(int authorId, BookAuthorVM bookAuthorVM)
+        {
+            int bookId = bookAuthorVM.Book.BookId;
+            BookAuthorMap bookAuthorMap = _db.BookAuthorMaps.FirstOrDefault(
+                u => u.Author_Id == authorId && u.Book_Id == bookId);
+
+
+            _db.BookAuthorMaps.Remove(bookAuthorMap);
+            _db.SaveChanges();
+            return RedirectToAction(nameof(ManageAuthors), new { @id = bookId });
+        }
+
+        public async Task<IActionResult> Playground()
+        {
+            IEnumerable<Book> BookList1 = _db.Books;
+            var FilteredBook1 = BookList1.Where(b => b.Price > 50).ToList();
+
+            IQueryable<Book> BookList2 = _db.Books;
+            var fileredBook2 = BookList2.Where(b => b.Price > 50).ToList();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
